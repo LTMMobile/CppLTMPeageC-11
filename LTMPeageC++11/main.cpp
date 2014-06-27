2a86b11082caae265b23459e3295607a55420758
Bertrand Leclercq - Mac Book Pro <bl@ltm.fr>
2014-06-27 11:07:41 +0200

    Initial Commit

---
+++ LTMPeageC++11/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LTMPeageC++11
+//
+//  Created by Bertrand LECLERCQ LTM on 27/06/2014.
+//  Copyright (c) 2014 LTM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
