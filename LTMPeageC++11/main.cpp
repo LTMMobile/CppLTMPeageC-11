af6b5c9d0d3676de59d14e9e0584e9d91af66d29
Bertrand Leclercq - Mac Book Pro <bl@ltm.fr>
2017-12-21 10:01:15 +0100

    commit main

--- LTMPeageC++11/main.cpp
+++ LTMPeageC++11/main.cpp
@@ -2,17 +2,58 @@
 //  main.cpp
 //  LTMPeageC++11
 //
-//  Created by Bertrand LECLERCQ LTM on 27/06/2014.
-//  Copyright (c) 2014 LTM. All rights reserved.
-//
 
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <random>
 
-int main(int argc, const char * argv[])
-{
+int const max_voiture = 256;
+std::mutex verrou_peage;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Thread
+void voiture(int n) {
+    verrou_peage.lock();
+    std::cout << "la voiture " << std::this_thread::get_id() << ", entre pour payer" << std::endl;
+    
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(1,6);
+    int dice_roll = distribution(generator);  // generates number in the range 1..6
+    
+    // random
+    static std::default_random_engine re {};
+    using Dist = std::uniform_int_distribution<int>;
+    static Dist uid {};
+    auto ms = std::chrono::milliseconds(uid(re, Dist::param_type{1000,5000}));
+    std::this_thread::sleep_for(ms);
+    
+    std::cout << "la voiture " << std::this_thread::get_id() << ", sort" << std::endl;
+    verrou_peage.unlock();
+}
+
+std::thread * tab[max_voiture];
+
+int main(int argc, const char * argv[]) {
+
+    int nb_voiture = 0;
+    std::cout << "Entrez le nombre de voiture ?" << std::endl;
+    std::cin >> nb_voiture;
+    
+    if( nb_voiture > 0 && nb_voiture < max_voiture ) {
+        //
+        for( int t = 0; t < nb_voiture; t++ ) {
+            tab[t] = new std::thread {std::bind(voiture, t)};
+        }
+        
+        for ( int i=0; i < nb_voiture; i++ ) {
+            tab[i]->join();
+            delete tab[i];
+            tab[i] = nullptr;
+        }
+    }else {
+        std::cout << "cas invalide" << std::endl;
+    }
+    
     return 0;
 }
 
